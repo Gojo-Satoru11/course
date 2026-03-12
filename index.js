6f96063472126473b05cffc08e0594f39516ab5e
Gojo-Satoru11 <alysaber.ntss@gmail.com>
2026-03-12 09:56:37 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// const btn = document.getElementById("like");
+// const label = ;
+let count = 0;
+let likes;
+document.getElementById("like").onclick = function clickme()
+{
+    count++ 
+    document.getElementById("lbl").textContent = "Likes: "+String(count)
+} ;
+open("likes.json") ;{
+    open("likes.json")
+}
